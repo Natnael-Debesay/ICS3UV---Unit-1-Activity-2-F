f702123391ac13fe1d17304804844941b44eba07
default <natnael.debesay@stu.ocsb.ca>
2025-10-02 17:30:59 +0000

    main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+/*
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-02
+ * @fileoverview This program displays the sum, difference, product, quotient, and modulo for my year of birth and the year I gradate.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	//string literal text
+	fmt.Println("2009 + 2027 = " + String(2009 + 2027))
+	
+	//string literal text
+	fmt.Println("2009 - 2027 = " + String(2009 - git a2027))
+
+	//string literal text
+	fmt.Println("2009 * 2027 = " + String(2009 * 2027))
+
+	//string literal text
+	fmt.Println("2009 / 2027 = "  + String(2009 / 2027))
+
+	//string literal text
+	fmt.Println("2009 % 2027 = " + String(2009 % 2027))
+
+	fmt.Println("\nDone.")
+}
---
+++ main.ts
@@ -0,0 +1,23 @@
+/**
+ * @author Natnael Debesay
+ * @version 1.0.0
+ * @date 2025-10-02
+ * @fileoverview This program displays the sum, difference, product, quotient, and modulo for my year of birth and the year I graduate.
+ */
+
+// string literal text
+console.log("2009 + 2027 = " + String(2009+2027));
+
+// string literal text
+console.log("2009 - 2027 = " + String(2009-2027));
+
+// string literal text
+console.log("2009 * 2027 = " + String(2009*2027));
+
+// string literal text
+console.log("2009 / 2027 = " + String(2009/2027));
+
+// string literal text
+console.log("2009 % 2027 = " + String (2009%2027));
+
+console.log("\nDone.");
